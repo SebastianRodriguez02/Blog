e7c5418a7fcaee94894ac6e2a925b525364fe989
SebastianRodriguez02 <sebastian3696@gmail.com>
2023-04-03 11:16:23 -0500

    Nuevo proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
 const { src, dest} = require ("gulp");
+const sass = require("gulp-sass") (require('sass'))
 
 function css (done) {
-    // Identifica el archivo SASS
-
-    // Compilar
-
-    // Almacena en disco
-
-    src('src/scss/app.scss').pipe()
+    src('src/scss/app.scss') // Identifica el archivo SASS
+      .pipe( sass() ) // Compilar
+      .pipe (dest("build/css")) // Almacena en disco
 
     done (); // callback que avisa a gulp cuando llegamos al final
 }
+
+exports.css = css;
